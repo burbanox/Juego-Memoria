a9fa8646d8d07f7a728cf069e69614320ad4d9a9
Harold Burbano <haroldburbano2002@gmail.com>
2021-05-09 17:17:11 -0400

    contador lluvia

--- archivo.js
+++ archivo.js
@@ -1,28 +1,11 @@
-var sujeto = 
-{
-    nombre : "Carlos Jimenez",
-    edad : 34,
-    estatura : 1.90,
-    peso : 80
-}
+const PROBABILIDAD_LLUVIA = 0.25
 
-function simulacion(persona,dias) 
-{
-    const PERDIDA_DIA = 0.65
+const estaLLoviendo = () => Math.random()<=PROBABILIDAD_LLUVIA
 
-    console.log(`Peson inicial : ${persona.peso}`)
-    for (var i = 0; i < dias; i++) {
-      var probabilidad = Math.random();
-      if (probabilidad <= 0.33) {
-        //Sube de peso
-        persona.peso+=PERDIDA_DIA*Math.random()
-      } else if (probabilidad <= 0.66) {
-        //Baja de peso
-        persona.peso-=PERDIDA_DIA*Math.random()
-      }
-      console.log(`Peso en el dia ${i} : ${persona.peso.toFixed(2)}kg`)
-    }
-    console.log(`Al final de ${dias} el peso de ${persona.nombre} es ${persona.peso.toFixed(2)}kg`)
-}
+var contador = 0 
 
-simulacion(sujeto, 365)
+do{
+    contador++
+}while(!estaLLoviendo())
+
+console.log(`Tuve que ir ${contador} hasta encontrar lluvia`)
