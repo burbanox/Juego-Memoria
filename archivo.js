d267bf3d3348b2160fa460b6e939f13204d3bf8d
Harold Burbano <haroldburbano2002@gmail.com>
2021-05-15 19:42:45 -0400

    Se trabajo con funciones como parametros

--- archivo.js
+++ archivo.js
@@ -20,7 +20,7 @@ class Desarrollador extends Persona
         super(nombre,apellido,altura)
     }
 
-    expertis()
+    expertis(fn)
     {
         var estatura;
         if(this.altura<1.80)
@@ -31,13 +31,23 @@ class Desarrollador extends Persona
             estatura = "soy grande"
         }
         console.log(`Hola soy ${this.nombre} ${this.apellido} ${estatura}`)
+
+        if(fn)
+        {
+            fn()
+        }
     }
+}
+
+function hacerCosa()
+{
+    alert("Yo no se que monda estare pagando")
 }
 
 var harold = new Desarrollador("Harold","Burbano",1.80)
 var valentina = new Desarrollador("Valentina","Burbanoo",1.60)
 
 harold.saludar()
-harold.expertis()
+harold.expertis(hacerCosa)
 valentina.saludar()
 valentina.expertis()
