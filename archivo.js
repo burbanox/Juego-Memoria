a0bd5003804972a22eeb5ba51787817d41722f9d
Harold Burbano <haroldburbano2002@gmail.com>
2021-05-20 20:49:38 -0400

    Se trabajo con promesas

--- archivo.js
+++ archivo.js
@@ -1,50 +1,49 @@
-const API_URL = 'https://swapi.dev/api/';
-var respuesta = prompt("Escribe : objeto/id");
+const API_URL = 'https://swapi.dev/api/people/';
 
-console.log("hola".green)
-
-function comprobarRespuesta(palabra)
+function obtenerDatos(id)
 {
-    var p = palabra.indexOf('/') + 1;
-    var comprobacion = false;
-    if(p!=0)
+    const API_URL = 'https://swapi.dev/api/people/';
+    var url = API_URL + id;
+    var obj = {crossDomain : true}
+    return new Promise((resolve,reject)=>
     {
-        var objeto = palabra.substr(0,p)
-        if(objeto==="people/" || objeto==="starships/" || objeto==="planets/")
+        $.get(url,obj,(data)=>
         {
-            var regex = /.*[0-9]+\/$/;
-            if(regex.test(palabra))
-            {
-                console.log('true')
-                comprobacion = true;
-            }
-        }
-    }
+            resolve(data,id)
+        }).fail(()=>reject(id))
+    })
+}
 
-    if(comprobacion)
+function obtenerPersonaje(id)
+{
+    obtenerDatos(id).then(saludar).catch(onError)
+}
+
+function saludar(personaje)
+{
+    console.log(`El id corresponde a : ${personaje.name}`)
+}
+
+function onError(id)
+{
+    console.log(`La id \#${id} no se encuentra registrada`);
+}
+
+function entrada()
+{
+    var id = prompt("Introduce el id de tu personaje : ");
+    obtenerPersonaje(id);
+}
+
+function interactividad()
+{
+    var eleccion = true; 
+    while(eleccion)
     {
-        return true
-    }else 
-    {
-        return false
+        entrada()
+        var respuesta = prompt("Deseas continuar ? : y/n")
+        eleccion = respuesta==='y';
     }
 }
 
-function buscarPersonaje(entrada)
-{
-    if(comprobarRespuesta(entrada))
-    {
-        var url = API_URL + entrada
-        var obj = { crossDomain : true }
-        $.get(url,obj,function(data)
-        {
-            console.log(`Esta id se trata de : ${data.name}`)
-        })
-    }
-    else
-    {
-        console.log("comando mal escrito")
-    }
-}
-
-buscarPersonaje(respuesta)
+interactividad();
