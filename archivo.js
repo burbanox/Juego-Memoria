8f8997b75476cec2930f87eaf183f64f08eb1d14
Harold Burbano <haroldburbano2002@gmail.com>
2021-05-26 01:02:36 -0400

    Se avanzo en el juego

--- archivo.js
+++ archivo.js
@@ -8,18 +8,197 @@ class Juego
 {
     constructor()
     {
+        this.elegirColor = this.elegirColor.bind(this); 
+        this.hoverColor = this.hoverColor.bind(this);
+        this.colorNormal = this.colorNormal.bind(this);
         this.inicializar();
+        this.generarSecuencia();
+        this.siguienteNivel();
     }
 
     inicializar() 
     {
         botonInicio.classList.add('hiden');
+        this.nivel = 1;
+        this.colores = 
+        {
+            redSection,
+            greenSection,
+            blueSection,
+            purpleSection
+        }
+
+        this.intervalo = 500; //intervalo entre encedido y apagado de colores
+        this.colores.blueSection.removeEventListener('onmouseover',this.hoverColor);
+        this.colores.redSection.removeEventListener('onmouseover',this.hoverColor);
+        this.colores.greenSection.removeEventListener('onmouseover',this.hoverColor);
+        this.colores.purpleSection.removeEventListener('onmouseover',this.hoverColor);
+    }
+
+    generarSecuencia() 
+    {
+        this.numeros = new Array(10).fill(0).map(n=> Math.floor(Math.random()*4))
+    }
+
+    transformarSecuencia(numero)
+    {
+        switch(numero)
+        {
+            case 0 :
+                return 'redSection';
+            case 1 :
+                return 'greenSection';
+            case 2 :
+                return 'blueSection';
+            case 3 :
+                return 'purpleSection';
+        }
+    }
+
+    siguienteNivel()
+    {
+        this.iluminarSecuencia();
+        this.agregarEventosClick();
+    }
+
+    iluminarSecuencia()
+    {
+        for(let i = 0;i<this.nivel;i++)
+        {
+            const color = this.transformarSecuencia(this.numeros[i]);
+            setTimeout(()=>this.iluminarColor(color),this.intervalo*2*i);
+        }
+        setTimeout(()=>this.agregarHover(),1000 + this.intervalo*2*this.nivel);
+    }
+    
+    iluminarColor(color)
+    {
+        switch(color)
+        {
+            case 'redSection':
+                this.colores[color].classList.add('sombreado-rojo');
+                break;
+            case 'greenSection':
+                this.colores[color].classList.add('sombreado-verde');
+                break;
+            case 'blueSection':
+                this.colores[color].classList.add('sombreado-azul');
+                break;
+            case 'purpleSection':
+                this.colores[color].classList.add('sombreado-purpura');
+                     break;
+        }
+        setTimeout(()=>this.apagarColor(color),this.intervalo);
+    }
+
+    agregarHover()
+    {
+        this.colores.blueSection.onmouseover = this.hoverColor;
+        this.colores.redSection.onmouseover = this.hoverColor;
+        this.colores.greenSection.onmouseover = this.hoverColor;
+        this.colores.purpleSection.onmouseover = this.hoverColor;
+        this.colores.blueSection.onmouseout = this.colorNormal
+        this.colores.redSection.onmouseout = this.colorNormal
+        this.colores.greenSection.onmouseout = this.colorNormal
+        this.colores.purpleSection.onmouseout = this.colorNormal
+    }
+
+    colorNormal(ev)
+    {
+        const COLOR = ev.target.dataset.color;
+        let colors;
+        switch(COLOR)
+        {
+            case 'rojo':
+                colors = 'redSection';
+                break;
+            case 'verde':
+                colors = 'greenSection';
+                break;
+            case 'azul':
+                colors = 'blueSection';
+                break;
+            case 'morado':
+                colors = 'purpleSection';
+                 break;
+        }
+        this.apagarColor(colors)
+    }
+
+    hoverColor(ev)
+    {
+        console.log('se esta haciendo hover sobre un elemento')
+        const COLOR = ev.target.dataset.color
+        switch(COLOR)
+        {
+            case 'rojo':
+                this.colores.redSection.classList.add('sombreado-rojo');
+                break;
+            case 'verde':
+                this.colores.greenSection.classList.add('sombreado-verde');
+                break;
+            case 'azul':
+                this.colores.blueSection.classList.add('sombreado-azul');
+                break;
+            case 'morado':
+                this.colores.purpleSection.classList.add('sombreado-purpura');
+                     break;
+        }
+
+    }
+
+    apagarColor(color)
+    {
+        switch(color)
+        {
+            case 'redSection':
+                this.colores[color].classList.remove('sombreado-rojo');
+                break;
+            case 'greenSection':
+                this.colores[color].classList.remove('sombreado-verde');
+                break;
+            case 'blueSection':
+                this.colores[color].classList.remove('sombreado-azul');
+                break;
+            case 'purpleSection':
+                this.colores[color].classList.remove('sombreado-purpura');
+                     break;
+        }
+    }
+
+    agregarEventosClick()
+    {
+        this.colores.blueSection.addEventListener('click',this.elegirColor)
+        this.colores.redSection.addEventListener('click',this.elegirColor)
+        this.colores.greenSection.addEventListener('click',this.elegirColor)
+        this.colores.purpleSection.addEventListener('click',this.elegirColor)
+    }
+
+    elegirColor(ev)
+    {
+        const COLOR_OBTENIDO = ev.target.dataset.color;
+        var numero = this.trasformarColorNumero(COLOR_OBTENIDO);
+    }
+
+    trasformarColorNumero(color)
+    {
+        switch(color)
+        {
+            case 'rojo' :
+                return 0;
+            case 'verde' :
+                return 1;
+            case 'azul' :
+                return 2;
+            case 'morado' :
+                return 3;
+        }
     }
 }
 
 function empezarJuego()
 {
-    var juego = new Juego()
+    let juego = new Juego()
 }
 
 function reiniciarJuego()
